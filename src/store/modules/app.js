0865dad0211c1be5b82ca7b93e6bf01ffeef7b8f
lamdx <513256514@qq.com>
2022-04-09 15:50:57 +0800

    feat：快捷编写方式，不建议使用

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,4 +1,3 @@
-
 const state = {
   userInfo: {}
 };
@@ -14,6 +13,19 @@ const actions = {
     commit('updateUserInfo', userInfo);
   }
 };
+
+// reduce 这种写法会破坏 vuex 的功能，不建议使用，因为 mutations getters 里面可以书写其他逻辑
+// const keysList = Object.keys(state);
+
+// const mutations = keysList.reduce(
+//   (prev, key) => ({ ...prev, [key]: (state, data) => (state[key] = data) }),
+//   {}
+// );
+
+// const getters = keysList.reduce(
+//   (prev, key) => ({ ...prev, [key]: state => state[key] }),
+//   {}
+// );
 
 export default {
   namespaced: true,
