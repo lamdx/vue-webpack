db8104811319931f90f4c577f6b44be253ea794d
lamdx <513256514@qq.com>
2022-04-09 19:51:18 +0800

    fix: axios baseURL

--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import qs from 'qs';
 
 // 基准 URL
-const baseURL = 'http://127.0.0.1:8080';
+const baseURL = 'http://localhost:8080';
 
 const instance = axios.create({
   baseURL,
